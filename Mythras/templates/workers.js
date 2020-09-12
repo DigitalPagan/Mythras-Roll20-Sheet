cd6a0d4e73462182e6ad6b8af501dfcb89b12374
Matthew Carpenter <matthew.d.carpenter@gmail.com>
2020-09-12 14:21:03 -0400

    Base templatizing done different sheet types

--- Mythras/templates/workers.js
+++ Mythras/templates/workers.js
@@ -1,1 +1,18 @@
+/* Sheet Mode Toggles */
+const modelist = ["play","edit","config"];
+modelist.forEach(modebutton => {
+    on(`clicked:${modebutton}`, function() {
+        setAttrs({
+            mode: modebutton
+        });
+    });
+});
+
+/* Sheet Type Specific Worker Scripts */
+{% if battle_units %}{% include 'mythras/battle_unit.js' %}{% endif %}
+{% include 'character/character.js' %}
+{% if ships %}{% include 'mythras/ship.js' %}{% endif %}
+{% if vehicles == 'mythras' %}{% include 'mythras/vehicle.js' %}{% endif %}
+{% if vehicles == 'frostbyte' %}{% include 'frostbyte/vehicle.js' %}{% endif %}
+{% if solar_systems %}{% include 'frostbyte/solar_system.js' %}{% endif %}
 
