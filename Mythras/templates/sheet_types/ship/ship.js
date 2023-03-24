5d6f04f932f418fb89af5bce688b410d327924af
Matthew Carpenter <matthew.d.carpenter@gmail.com>
2023-03-24 15:56:18 -0400

    Adding weapon conversion to ships

--- Mythras/templates/sheet_types/ship/ship.js
+++ Mythras/templates/sheet_types/ship/ship.js
@@ -50,7 +50,7 @@ on('change:ship_condition', function(event) {
 
     getAttrs(['ship_condition', 'ship_seaworthiness'], function (v) {
         const seaworthiness = parseInt(v['ship_seaworthiness']) || 0;
-        let range = Math.ceil((seaworthiness/10) * shipConditionTable[v['ship_condition']]['range']);
+        let range = Math.ceil((seaworthiness / 10) * shipConditionTable[v['ship_condition']]['range']);
         range = range < 0 ? 0 : range;
 
         setAttrs({
@@ -63,21 +63,39 @@ on('change:ship_condition', function(event) {
 });
 
 function upgradeShip3Dot0() {
-    getAttrs(['ship_notes', "ship_seaworthiness_penalty", "ship_seaworthiness", "ship_condition"], function (v) {
-        let newAttrs = {'version': '3.0'};
+    getSectionIDs("repeating_shiparmament", function(shiparmamentIds) {
+        let shiparmamentGetAttrs = [];
+        shiparmamentIds.forEach(id => {
+            shiparmamentGetAttrs.push(`repeating_shiparmament_${id}_name`, `repeating_shiparmament_${id}_range`, `repeating_shiparmament_${id}_damage`, `repeating_shiparmament_${id}_load`, `repeating_shiparmament_${id}_notes`, `repeating_shiparmament_${id}_ap`, `repeating_shiparmament_${id}_hp`);
+        });
+        getAttrs(shiparmamentGetAttrs.concat(['ship_notes', "ship_seaworthiness_penalty", "ship_seaworthiness", "ship_condition"]), function (v) {
+            let newAttrs = {'version': '3.0'};
 
-        const seaworthiness = parseInt(v['ship_seaworthiness']) || 0;
-        const seaworthiness_penalty = parseInt(v['ship_seaworthiness_penalty']) || 0;
-        newAttrs["ship_seaworthiness"] = seaworthiness - seaworthiness_penalty;
+            const seaworthiness = parseInt(v['ship_seaworthiness']) || 0;
+            const seaworthiness_penalty = parseInt(v['ship_seaworthiness_penalty']) || 0;
+            newAttrs["ship_seaworthiness"] = seaworthiness - seaworthiness_penalty;
 
-        let range = Math.ceil((newAttrs["ship_seaworthiness"]/10) * shipConditionTable[v['ship_condition']]['range']);
-        newAttrs["ship_range"] = range < 0 ? 0 : range;
+            let range = Math.ceil((newAttrs["ship_seaworthiness"]/10) * shipConditionTable[v['ship_condition']]['range']);
+            newAttrs["ship_range"] = range < 0 ? 0 : range;
 
-        /* Convert Notes */
-        if (v['ship_notes']) {
-            newAttrs['sheet_notes'] = v['ship_notes'];
-        }
+            shiparmamentIds.forEach(id => {
+                const weaponId = 'repeating_rangedweapon_' + generateRowID();
+                newAttrs[`${weaponId}_name`] = v[`repeating_shiparmament_${id}_name`] ? v[`repeating_shiparmament_${id}_name`] : "?";
+                newAttrs[`${weaponId}_damage`] = v[`repeating_shiparmament_${id}_damage`] ? v[`repeating_shiparmament_${id}_damage`] : "";
+                newAttrs[`${weaponId}_range`] = v[`repeating_shiparmament_${id}_range`] ? v[`repeating_shiparmament_${id}_range`] : "";
+                newAttrs[`${weaponId}_load`] = v[`repeating_shiparmament_${id}_load`] ? v[`repeating_shiparmament_${id}_load`] : "";
+                newAttrs[`${weaponId}_notes`] = v[`repeating_shiparmament_${id}_notes`] ? v[`repeating_shiparmament_${id}_notes`] : "";
+                newAttrs[`${weaponId}_ap`] = v[`repeating_shiparmament_${id}_ap`] ? v[`repeating_shiparmament_${id}_ap`] : "0";
+                newAttrs[`${weaponId}_hp`] = v[`repeating_shiparmament_${id}_hp`] ? v[`repeating_shiparmament_${id}_hp`] : "0";
+                newAttrs[`${weaponId}_hp_max`] = v[`repeating_shiparmament_${id}_hp`] ? v[`repeating_shiparmament_${id}_hp`] : "0";
+            });
 
-        setAttrs(newAttrs);
+            /* Convert Notes */
+            if (v['ship_notes']) {
+                newAttrs['sheet_notes'] = v['ship_notes'];
+            }
+
+            setAttrs(newAttrs);
+        });
     });
 }
